151b7c28836e04bd48f924ae7246da75a339d695
Bao Nguyen <bao.h.nguyen12@gmail.com>
2025-09-01 18:16:42 -0500

    added terminal bg image

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -11,6 +11,18 @@ config.font_size = 12
 -- Color scheme
 config.color_scheme = "Sequoia Moonlight"
 
+-- set the background image
+local config_dir = wezterm.config_dir
+
+config.window_background_image = config_dir .. "/bg_image.jpg"
+
+-- optional: adjust brightness, hue, saturation
+config.window_background_image_hsb = {
+	brightness = 0.1, -- 0 = very dark, 1 = normal
+	hue = 1.0,
+	saturation = 0.1,
+}
+
 -- Tab bar
 config.hide_tab_bar_if_only_one_tab = true
 
